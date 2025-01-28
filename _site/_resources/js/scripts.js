2eb3697d68354918eb9374abccebebc30a973f79
Harvey Lieberman <harvey.lieberman@gmail.com>
2025-01-27 21:39:45 -0500

    render

---
+++ _site/_resources/js/scripts.js
@@ -0,0 +1,11 @@
+  document.addEventListener("DOMContentLoaded", function() {
+    const counts = document.querySelectorAll('.quarto-category-count');
+    counts.forEach(count => {
+      let text = count.textContent;
+      if (text.length > 1) {
+        count.textContent = text.slice(1, -1);
+      } else {
+        count.textContent = '';
+      }
+    });
+  });  
